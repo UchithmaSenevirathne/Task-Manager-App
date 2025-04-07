246fc18893c95437139fb18b4fb88975d0f5c3c8
UchithmaSenevirathne <uchithma@gmail.com>
2025-04-07 09:53:51 +0530

    add bootstrap css

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
