22e30b27986051db1e50abcde54619e7fa0e7ea9
UchithmaSenevirathne <uchithma@gmail.com>
2025-04-09 20:09:39 +0530

    create navbar for user

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared/shared.module';
 import { MatIconModule } from '@angular/material/icon';
+import { StorageService } from './auth/services/storage/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,19 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class AppComponent {
   title = 'frontend';
+
+  isLoggedIn: boolean = StorageService.isLoggedIn();
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe(event => {
+      this.isLoggedIn = StorageService.isLoggedIn();
+    })
+  }
+
+  logout(){
+    StorageService.signout();
+    this.router.navigateByUrl("");
+  }
 }
